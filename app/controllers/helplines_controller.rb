623e1770dfe073e1a8ee571f3c1f71384a9bce6a
Zoe Rose zoealexandrarose@outlook.com <zoealexandrarose@outlook.com>
2022-11-28 17:08:10 +0100

    added extra controller

---
+++ app/controllers/helplines_controller.rb
@@ -0,0 +1,2 @@
+class HelplinesController < ApplicationController
+end
---
+++ test/controllers/helplines_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class HelplinesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
