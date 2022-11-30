be86d6ee74f9b2dfda6a999b652d7dbc6a6d406a
Rose van der Giessen <rosevandergiessen@proton.me>
2022-11-30 14:29:47 +0100

    add map and user-address

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, only: [ :home ]
+  before_action :authenticate_user!, only: [ :home, :map ]
 
   def home
   end
+
+  # def map
+  #   @marker = {
+  #     lat: current_user.latitude,
+  #     lng: current_user.longitude
+  #   }
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,13 @@ class User < ApplicationRecord
   has_many :messages
   has_many :friends
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :phone_number, presence: true
+  validates :address, presence: true
   validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
-
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
 end
